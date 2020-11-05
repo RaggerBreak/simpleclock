57f91ccd386069f35fcfd05161275359301ccfd7
RaggerLtp <ragger.break@gmail.com>
2020-11-05 20:43:25 +0100

    init

---
+++ src/main/java/com/simpleclock/SimpleclockApplication.java
@@ -0,0 +1,13 @@
+package com.simpleclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleclockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpleclock/SimpleclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
